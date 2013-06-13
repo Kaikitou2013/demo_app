a776bbca990787a51bc4d472d7efa7b8ba23a531
rubyapptest <kaikitou-aws-cloud@ml.css.fujitsu.com>
2013-06-13 04:31:52 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'd1954e40ce04ac4408296c1f8ffb29caed0e4a577cb0df0b6edf3fb978b7dd361260f8f18af0f391fba3d32bc30e609e643541c389a9f1883b65432750d14992'
